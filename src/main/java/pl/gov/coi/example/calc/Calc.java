3b2dc00770d593d826afec26ca3dd27e83fcc6d0
Pawel Banach <pawel.banach@ingbank.pl>
2016-12-14 09:31:08 +0100

    zmiana

--- src/main/java/pl/gov/coi/example/calc/Calc.java
+++ src/main/java/pl/gov/coi/example/calc/Calc.java
@@ -7,7 +7,13 @@ package pl.gov.coi.example.calc;
 public class Calc {
 
     public int add(int a, int b) {
+      if (a > b) {
         return a + b;
+      } else if (b < a) {
+        return b + a;
+      } else {
+        return 2 * a;
+      }
     }
     public int substract(int a, int b) {
         return a - b;
